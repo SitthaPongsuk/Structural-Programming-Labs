f78e34472d441533041b798f71b0ed931686e5c0
SitthaPongsuk <s6606021421233@email.kmutnb.ac.th>
2023-07-17 22:38:19 +0700

    Create work2.cpp

---
+++ work2.cpp
@@ -0,0 +1,52 @@
+/* 2_4 Program to calculate area of ​​rectangle  */
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num1, num2;
+//     cout << "Program to calculate area of rectangle" << endl ;
+//     cout << "Enter Length : ";
+//     cin >> num1;
+//     cout << "Enter Widht : ";
+//     cin >> num2;
+//     cout << endl;
+//     cout << "Area of Rectagle = " << num1 * num2 << endl;
+//     return(0);
+// }
+
+
+
+/* 2_5 Program to calculate area of Circle */
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     float num1;
+//     cout << "Program to calculate area of Circle" << endl;
+//     cout << "Circle radius (real number) ? ";
+//     cin >> num1;
+//     cout << endl;
+//     cout << "Area of circle with radius" << " " << num1 << " " << "is" << " " << 3.1415 * num1 * num1 << endl;
+//     cout << "Circumference is" << " " << 2 * 3.1415 * num1 << endl;
+//     return(0);
+// }
+
+
+
+/* 2_6 Program to receive product price and VAT rate (%) */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Enter price : ";
+    cin >> num1;
+    cout << "Enter vat (%) : ";
+    cin >> num2;
+    cout << endl;
+    cout << "Net Price of product = " << " " << num1 + (num1 * num2 / 100) << endl;
+    return(0);
+}
